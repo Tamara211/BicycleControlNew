ac71bc87883b5214c5f61fba05098932f55029cb
ga92xuv <ga92xuv@mytum.de>
2017-07-07 11:43:35 +0200

    Added calculation for steering torque

--- BicycleControl/EquationsCalculation.py
+++ BicycleControl/EquationsCalculation.py
@@ -4,10 +4,19 @@ from BicycleParameters import BicycleParameters
 from ConstantMatrices import ConstantMatrices
 
 class EquationsCalculation:
+
+    kp = 4.28  # TODO validate value - Constant value in torque calculation
+
     def __init__(self):
 
         self.params = BicycleParameters()
         self.matrices = ConstantMatrices()
+        self.deltaTorque # Steering torque
+        self.desiredDeltaAngle #TODO recieve from visualization
+
+
+    def calculateTorque(self):
+        self.deltaTorque = self.kp*(self.params.delta - self.desiredDeltaAngle) #TODO is it ok to ignore the kd(deltaDot) ?
 
     def kinematicEquations(self):
         self.params.x = self.params.v * cos(self.params.psi) * self.params.rearWheelRadius
@@ -16,3 +25,4 @@ class EquationsCalculation:
 
 
     def dynamicEquations(self):
+
