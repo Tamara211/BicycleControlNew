4c122ebe2a83b2e2b5eeca1e4a8a13b7183d0dc9
ga92xuv <ga92xuv@mytum.de>
2017-07-13 15:27:22 +0200

    Implemented Controller

--- BicycleControl/Controller.py
+++ BicycleControl/Controller.py
@@ -1,2 +1,17 @@
 from EquationsCalculation import EquationsCalculation
 
+class Controller:
+
+
+    def __init__(self):
+        self.result = EquationsCalculation()
+
+    def requestSteer(self, desiredSteeringAngle):
+        resultedRollAngle = self.result.calculateRollAngleAfterSteering(desiredSteeringAngle)
+        if (resultedRollAngle > 0.5 or resultedRollAngle < -0.5):  # TODO validate values
+            return 0  # Cannot steer
+        else:  #  allow steering
+             return 1  # Can steer
+
+
+
