c288e4e2d5cb7173d3c54ca0516ab14f8d017da1
ga25vid <fabian.kloos@tum.de>
2017-09-14 19:31:10 +0200

    set up basic controller 2nd try

--- BicycleControl/Controller.py
+++ BicycleControl/Controller.py
@@ -1,21 +1,41 @@
 from EquationsCalculation import EquationsCalculation
 
 class Controller:
-
-
-    def __init__(self):
-        self.result = EquationsCalculation()
-        #input from GUI
-        self.maxRollAngle = 0
-        self.minRollAngle = 0
-        self.desiredSteerAngle = 0
-
-    def requestSteer(self):
-        resultedRollAngle = self.result.calculateRollAngleAfterSteering(self.desiredSteeringAngle)
-        if (resultedRollAngle > self.maxRollAngle or resultedRollAngle < self.minRollAngle):
-            return 0  # Cannot steer
-        else:  #  allow steering
-             return 1  # Can steer
+		
+	def __init__(self):
+		self.result = EquationsCalculation()
+		#input from GUI
+		self.maxRollAngle = 0
+		self.minRollAngle = 0
+		self.desiredSteerAngle = 0
+             
+    while True:
+		if (fallingOver):
+			#TODO write reaction method
+			
+		if (newGUIInput):
+			#TODO new velocity input? adjust desired velocity and map motor values
+			#TODO requestSteer check if steering possible or allowed
+			#TODO map dynamixel values and send to dynamixel
+			
+		if (obstacleAhead):
+			#TODO react to obstacle
+			
+		#TODO send motor value to motor
+		
+		
+	def requestSteer(self):
+		resultedRollAngle = self.result.calculateRollAngleAfterSteering(self.desiredSteeringAngle)
+		if (resultedRollAngle > self.maxRollAngle or resultedRollAngle < self.minRollAngle):
+			return 0  # Cannot steer
+		else:  #  allow steering
+			return 1  # Can steer
+			
+	def fallingOver(self):
+		return True
+		
+	def obstacleAhead(self):
+		return True
 
 
 
